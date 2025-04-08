6edefdd3c544c2ff61a672cda79497df26d71f13
LeoRocha <teengameplay883@gmai.com>
2025-04-07 22:05:52 -0300

    crei o site

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let input = document.getElementById("input");
+let btnAdd = document.getElementById("btn_add");
+let main = document.getElementById("area-main")
+function addTarefa() {
+    let valorInput = input.value;
+    if ((valorInput !== 0) && (valorInput !== null) && (valorInput !== "") && (valorInput !== undefined)) {
+        let NovoItem = `
+        <main id="area-main">
+            <div id="lista-feita">
+                <div id="icone-v"><i class="mdi mdi-circle-outline"></i></div>
+                <div id="barra-t">
+                    ${valorInput}
+                </div>
+                <button id="btn-d">
+                    <i class="mdi mdi-delete"></i>
+                </button>
+            </div>
+        </main>
+    `
+        main.innerHTML += NovoItem;
+
+    }
+}
