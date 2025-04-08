d747435788c0a279f2d2e6236226f15f7cf484e7
LeoRocha <teengameplay883@gmai.com>
2025-04-08 11:27:45 -0300

    nova atualização

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@ let main = document.getElementById("area-main")
 function addTarefa() {
     let valorInput = input.value;
     if ((valorInput !== 0) && (valorInput !== null) && (valorInput !== "") && (valorInput !== undefined)) {
-        let NovoItem = `
-        <main id="area-main">
-            <div id="lista-feita">
-                <div id="icone-v"><i class="mdi mdi-circle-outline"></i></div>
-                <div id="barra-t">
-                    ${valorInput}
-                </div>
-                <button id="btn-d">
-                    <i class="mdi mdi-delete"></i>
-                </button>
+        let NovoItem = `<main id="area-main">
+        <div id="lista-feita">
+            <div id="icone-v"><i class="mdi mdi-circle-outline"></i></div>
+            <div id="barra-t">
+                ${valorInput}
             </div>
-        </main>
+            <button id="btn-d">
+                <i class="mdi mdi-delete"></i>
+            </button>
+        </div>
+    </main>
+
     `
         main.innerHTML += NovoItem;
 
