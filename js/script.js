304ca54221c0c0869802efa766c7d210034b6986
LeoRocha <teengameplay883@gmai.com>
2025-04-09 11:01:11 -0300

    correção do js;

--- js/script.js
+++ js/script.js
@@ -1,23 +1,76 @@
+let contador = 0;
 let input = document.getElementById("input");
-let btnAdd = document.getElementById("btn_add");
+let btnAdd = document.querySelector("#btn_add");
 let main = document.getElementById("area-main")
+
+
+
 function addTarefa() {
     let valorInput = input.value;
     if ((valorInput !== 0) && (valorInput !== null) && (valorInput !== "") && (valorInput !== undefined)) {
-        let NovoItem = `<main id="area-main">
-        <div id="lista-feita">
+        contador++;
+        let NovoItem = `
+        <div  id="${contador}" class ="lista-feita">
             <div id="icone-v"><i class="mdi mdi-circle-outline"></i></div>
             <div id="barra-t">
                 ${valorInput}
             </div>
-            <button id="btn-d">
+            <button class="btn-d" onclick="deletar(${contador})">
                 <i class="mdi mdi-delete"></i>
             </button>
         </div>
-    </main>
-
     `
         main.innerHTML += NovoItem;
-
+        input.value = "";
+        input.focus();
     }
 }
+
+function deletar(id) {
+    let tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let icone = document.getElementById("icone-v");
+function onclique() {
+    let NovoItem = `<main id="area-main">
+    <div id="lista-clique">
+            <div id="icone-v">
+                <i class="mdi mdi-check-underline-circle"></i>
+            </div>
+            <div id="barra-t">
+            ${valorInput
+        }
+            </div>
+            <button id="btn-d"><i class="mdi mdi-delete"></i></button>
+        </div>
+</main>
+
+`
+}
+*/
+
+
+console.log(contador);
